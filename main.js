ea29862ce2cb20c0c68e413ed96dbf4b11271d9c
2BarrCoder <125504293+2BarrCoder@users.noreply.github.com>
2024-09-19 13:42:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
+const panoramaImage = new PANOLENS.ImagePanorama("images/aerial-drone-panoramic-view-old-brasov-centre-romania.jpg");
 const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
